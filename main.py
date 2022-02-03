8b4fec1eaee1758f14ece11f54ca724ca63c5c89
SaainOne2035 <97127496+SaainOne2035@users.noreply.github.com>
2022-02-03 17:24:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import pytest
+
+class MainClass():
+    def __init__(self, getLocalNumber):
+        self.getLocalNumber = getLocalNumber
+
---
+++ test_file.py
@@ -0,0 +1,6 @@
+from main import *
+
+
+def test_main_class():
+    test_main_class = MainClass(14)
+    assert test_main_class.getLocalNumber == 14
